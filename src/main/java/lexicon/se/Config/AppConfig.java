91c56ce1601a489cc06e98ca5f6b187ed14c84ef
Mtaha55 <38652177+Mtaha55@users.noreply.github.com>
2024-05-23 12:17:47 +0200

    Workshop-DependencyInjection-part1
    Workshop-DependencyInjection-part1

---
+++ src/main/java/lexicon/se/Config/AppConfig.java
@@ -0,0 +1,12 @@
+package lexicon.se.Config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages = "lexicon.se")
+public class AppConfig {
+    // Other configurations if necessary
+}
